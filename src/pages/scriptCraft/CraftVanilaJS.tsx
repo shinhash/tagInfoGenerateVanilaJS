097b5c1706b1b1222e9b0073a0a8de725bbe0a2c
shinhash <shinhash123@gmail.com>
2026-02-16 12:29:29 +0900

    code clean set commit shs 20260216

--- src/pages/scriptCraft/CraftVanilaJS.tsx
+++ src/pages/scriptCraft/CraftVanilaJS.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "../../css/pages/scriptCraft/ScriptCraft.css";
 import { ClipboardCopy, createHandleChange } from "../../common/utils";
 
